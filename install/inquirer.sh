5f1e6fa42fa1fde3882707c8c938d30164596df6
Stumie <Stumie123@googlemail.com>
2026-02-13 23:29:02 +0100

    tried yet another variant of install/inquirer.sh

--- install/inquirer.sh
+++ install/inquirer.sh
@@ -1,57 +1,47 @@
 #!/usr/bin/env bash
-# Copyright (c) 2024 kahkhang
-# All rights reserved.
-#
-# SPDX-License-Identifier: MIT
-# For original source, see https://github.com/kahkhang/Inquirer.sh
 
 ### GLOBAL CONSTANTS ###
-declare -r ANSI_LIGHT_BLUE="\033[1;94m" # Light blue text.
-declare -r ANSI_LIGHT_GREEN="\033[92m"  # Light green text.
-declare -r ANSI_CLEAR_TEXT="\033[0m"    # Default text.
+declare -r ANSI_LIGHT_BLUE="\033[1;94m"
+declare -r ANSI_LIGHT_GREEN="\033[92m"
+declare -r ANSI_CLEAR_TEXT="\033[0m"
 
 ### FUNCTIONS ###
 function inqMenu() {
-    # DECLARE VARIABLES.
-    declare DIALOG_TEXT="$1"         # Dialog heading.
-    declare -n INPUT_OPTIONS="$2"    # Input variable name.
-    declare -n RETURN_STRING="$3"    # Output variable name.
+    local DIALOG_TEXT="$1"
+    local -n INPUT_OPTIONS="$2"
+    local -n RETURN_STRING="$3"
 
-    # MAIN LOGIC.
-    # Create a menu using kdialog
-    local OPTIONS_STR=""
+    local OPTIONS=()
     local i=1
     for OPTION in "${INPUT_OPTIONS[@]}"; do
-        OPTIONS_STR+="$i \"$OPTION\" "
+        OPTIONS+=("$i" "$OPTION")
         ((i++))
     done
 
-    local SELECTED_OPTION=$(kdialog --menu "$DIALOG_TEXT" --separate-output $OPTIONS_STR --title "Menu" 2>/dev/null) || exit 0
+    local SELECTED_OPTION=$(kdialog --menu "$DIALOG_TEXT" "${OPTIONS[@]}" --title "Menu" 2>/dev/null)
+    if [ $? -ne 0 ]; then
+        exit 0
+    fi
 
-    # Map the selected number back to the option text
-    local index=$((SELECTED_OPTION - 1))
-    RETURN_STRING="${INPUT_OPTIONS[index]}"
+    RETURN_STRING="${INPUT_OPTIONS[$((SELECTED_OPTION - 1))]}"
 
-    # Display question and response.
     echo -e "${ANSI_LIGHT_GREEN}Q) ${ANSI_CLEAR_TEXT}${ANSI_LIGHT_BLUE}${DIALOG_TEXT}${ANSI_CLEAR_TEXT} --> ${ANSI_LIGHT_GREEN}${RETURN_STRING}${ANSI_CLEAR_TEXT}"
 }
 
 function inqChkBx() {
-    # DECLARE VARIABLES.
-    declare DIALOG_TEXT="$1"          # Dialog heading.
-    declare -n INPUT_OPTIONS="$2"     # Input variable name.
-    declare -n RETURN_ARRAY="$3"      # Output variable name.
+    local DIALOG_TEXT="$1"
+    local -n INPUT_OPTIONS="$2"
+    local -n RETURN_ARRAY="$3"
 
-    # MAIN LOGIC.
-    # Create a checklist using kdialog
-    local OPTIONS_STR=""
+    local OPTIONS=()
     for OPTION in "${INPUT_OPTIONS[@]}"; do
-        OPTIONS_STR+="$OPTION \"$OPTION\" off "
+        OPTIONS+=("$OPTION" "$OPTION" "off")
     done
 
-    local SELECTED_OPTIONS_STRING=$(kdialog --checklist "$DIALOG_TEXT" $OPTIONS_STR --title "Checkbox" 2>/dev/null) || exit 0
+    local SELECTED_OPTIONS=$(kdialog --checklist "$DIALOG_TEXT" "${OPTIONS[@]}" --title "Checkbox" 2>/dev/null)
+    if [ $? -ne 0 ]; then
+        exit 0
+    fi
 
-    # Convert the output string into an array.
-    local IFS=$'\n'
-    RETURN_ARRAY=($(echo "$SELECTED_OPTIONS_STRING" | sed 's/^"//;s/"$//'))
+    IFS=$'\n' read -ra RETURN_ARRAY <<< "$SELECTED_OPTIONS"
 }
